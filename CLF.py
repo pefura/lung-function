85f5589d93d046295d38716dc195f000f8293a33
pefura <88545079+pefura@users.noreply.github.com>
2022-10-23 19:17:42 +0100

    Update CLF.py

--- CLF.py
+++ CLF.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-
+st.set_page_config(
+    layout="centered", page_icon="random"
+)
 # App title
 st.write("""
 # Wellcome to Cameroonian's lung function  calculator App
@@ -217,7 +219,7 @@ spiro = np.array(spiro).reshape(4,5)
 spiro_df = pd.DataFrame(spiro).set_axis(['Measured', 'Predicted', 'LLN', 'Z-score', '%Predicted'], axis='columns')
 spiro_df = spiro_df.rename(index={0: "FEV1,L", 1:"FVC,L", 2:"FEV1/FVC", 3:"FEF25-75%,L/s"})
 st.write(spiro_df)
-st.text(""" FEV1 = forced expiratory volume in 1s ; FVC = forced vital capacity; 
+st.caption(""" FEV1 = forced expiratory volume in 1s ; FVC = forced vital capacity; 
 FEV1/FVC = FEV1/FVC ratio; FEF25-75% = forced mid-expiratory flow; 
 LLN = lower limit of normal
 """)
